1468370591e5c7b06a98c94c6d1cf349ecf54ef9
Scott Trenda <Scott.Trenda@sportngin.com>
2014-05-13 19:54:56 -0500

    push the things

--- trials.js
+++ trials.js
@@ -1,4 +1,6 @@
 var trials = require('trial-client/index')
+var http = require('http')
+var async = require('async')
 
 // set the host name of the trials server:
 trials.serverHost = 'http://10.0.1.99:3001'
@@ -11,8 +13,27 @@ trials.teamName = 'Team Red' // Your Team Name
 
 // You can write them all in this file:
 trials.add(function(options, callback) {
+  // HAHA I FOUND A BUG
   callback()
 })
+
+trials.add(function(urls, callback) {
+  // Provide a function that takes two arguments, `urls` (array) and `callback`. Make an HTTP GET
+  // request to each url in the `urls` array, and place each response body result
+  // into a new array at the same position the request's url is in it's array.
+  // This is essentially an asynchronous version of `Array.prototype.map`. Invoke
+  // the callback with the resulting array (error first!).
+
+  async.map(urls, function(url, callback) {
+    var body = ''
+    http.get(url, function(res) {
+      res.on('data', function(chunk) { body += chunk })
+      res.on('end', function() { callback(null, body) })
+    })
+  }, callback)
+})
+
+
 
 // Or in multiple files (recommended):
 // trials.add(require('./trial1'))
